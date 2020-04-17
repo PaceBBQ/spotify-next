08f1e59aacc49f5f972c591c1652068dc73c23c2
PaceBBQ <33751086+PaceBBQ@users.noreply.github.com>
2020-04-17 14:42:22 +0300

    Update spotify-api.js
    removed client id & secret

--- api/spotify-api.js
+++ api/spotify-api.js
@@ -8,8 +8,8 @@ let prod = true;
 const scopes = "user-read-private+user-read-email+playlist-read-private+user-top-read+user-read-recently-played";
 const redirectURI = prod ? "https://next-spotify.herokuapp.com/spotify" : "http://localhost:8080/spotify";
 
-export const clientID = "47b847e2aa9c4a73b248e1ad79ed5529";
-export const clientSecret = "258a87ab76574483ada75e425d25a7e9";
+export const clientID = "";
+export const clientSecret = "";
 export const spotifyWebApiURL = `https://accounts.spotify.com/authorize/?client_id=${clientID}&response_type=token&redirect_uri=${redirectURI}&scope=${scopes}`;
 export const spotifyProfileURL = "https://api.spotify.com/v1/me?access_token=";
 export const spotifyPlaylistURL = "https://api.spotify.com/v1/me/playlists?access_token=";
